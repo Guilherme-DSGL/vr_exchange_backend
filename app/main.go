108464cb587e036920e3cd2e800ebdccbc8dd9de
Guilherme de Souza <guilermeleitaoo@gmail.com>
2025-04-26 19:28:06 -0300

    create main.go

--- app/main.go
+++ app/main.go
@@ -1,5 +1,82 @@
-package app
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	transactionServ "github.com/Guilherme-DSGL/purchase_transaction_backend/domain/services/transaction"
+	postgressRepo "github.com/Guilherme-DSGL/purchase_transaction_backend/internal/repository/postgress"
+	"github.com/Guilherme-DSGL/purchase_transaction_backend/rest/middleware"
+	transactionRest "github.com/Guilherme-DSGL/purchase_transaction_backend/rest/transaction"
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	_ "github.com/lib/pq"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error .env file not found")
+	}
+}
 
 func main() {
+	dbConn := initDb()
+	defer func() {
+		err := dbConn.Close()
+		if err != nil {
+			log.Fatal("error when closing DB", err)
+		}
+	}()
 
+	e := echo.New()
+	e.Use(middleware.CORS)
+	connectionTimeout := os.Getenv("CONNECTION_TIMEOUT")
+
+	timeout, err := strconv.Atoi(connectionTimeout)
+	if err != nil {
+		log.Fatal("timout failed to parse as int")
+		return
+	}
+
+	timeoutDuration := time.Duration(timeout) * time.Second
+	e.Use(middleware.SetRequestContextTimeout(timeoutDuration))
+
+	initDependencies(dbConn, e)
+
+	address := os.Getenv("SERVER_ADDRESS")
+	log.Fatal(e.Start(address))
+}
+
+func initDb() *sql.DB {
+	dbHost := os.Getenv("HOST_DATABASE")
+	dbPort := os.Getenv("PORT_DATABASE")
+	dbUser := os.Getenv("USER_DATABASE")
+	dbPass := os.Getenv("PASS_DATABASE")
+	dbName := os.Getenv("NAME_DATABASE")
+
+	connection := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", dbUser, dbPass, dbHost, dbPort, dbName)
+
+	dbConn, err := sql.Open(`postgres`, connection)
+	if err != nil {
+		log.Fatal("failed to open connection to database", err)
+	}
+	err = dbConn.Ping()
+	if err != nil {
+		log.Fatal("failed to ping database ", err)
+	}
+	return dbConn
+}
+
+func initDependencies(dbConn *sql.DB, e *echo.Echo) {
+	//  Repositories
+	transactioRepo := postgressRepo.NewTransactionRepository(dbConn)
+
+	// Services
+	svc := transactionServ.NewTransactionService(transactioRepo)
+	transactionRest.NewTransactionHandler(e, svc)
 }
